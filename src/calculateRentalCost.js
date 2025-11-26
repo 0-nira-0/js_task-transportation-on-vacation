78cf629d3ad6d6f164f99326173d9e63622ab19b
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2025-11-26 07:44:40 +0100

    fix solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,17 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const rentalCostForDays = 40 * days;
+  const COST_PER_DAY = 40;
+  const LONG_TERM_RENTAL_DAYS = 6;
+  const LONG_TERM_DISCOUNT = 50;
+  const COUNT_COST = COST_PER_DAY * days
 
-  if (days > 6) {
-    return rentalCostForDays - 50;
+  if (days > LONG_TERM_RENTAL_DAYS) {
+    return COUNT_COST - LONG_TERM_DISCOUNT;
   }
 
   if (days > 2) {
-    return rentalCostForDays - 20;
+    return COUNT_COST - 20;
   }
 
-  return rentalCostForDays;
+  return COUNT_COST;
 }
 
 module.exports = calculateRentalCost;

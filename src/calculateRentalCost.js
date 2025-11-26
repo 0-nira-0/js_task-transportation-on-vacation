0553efff6c0c20132852eb229f5c88f5ddc97dd0
Mykyta Oliinyk <mykyta.oliinyk0@gmail.com>
2025-11-26 08:19:58 +0100

    finaly solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,14 +7,16 @@ function calculateRentalCost(days) {
   const COST_PER_DAY = 40;
   const LONG_TERM_RENTAL_DAYS = 6;
   const LONG_TERM_DISCOUNT = 50;
-  const COUNT_COST = COST_PER_DAY * days
+  const COUNT_COST = COST_PER_DAY * days;
+  const SHORT_TERM_RENTAL_DAYS = 2;
+  const SHORT_TERM_DICSOUNT = 20;
 
   if (days > LONG_TERM_RENTAL_DAYS) {
     return COUNT_COST - LONG_TERM_DISCOUNT;
   }
 
-  if (days > 2) {
-    return COUNT_COST - 20;
+  if (days > SHORT_TERM_RENTAL_DAYS) {
+    return COUNT_COST - SHORT_TERM_DICSOUNT;
   }
 
   return COUNT_COST;
